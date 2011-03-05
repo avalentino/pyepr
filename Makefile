3ac262f0613ccef9d785e2678c942cc50a50dab4
Antonio Valentino <antonio.valentino@tiscali.it>
2011-03-05 19:43:42 +0100

    Improved setup script

--- Makefile
+++ Makefile
@@ -1,4 +1,22 @@
 #!/usr/bin/make -f
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011, Antonio Valentino <antonio.valentino@tiscali.it>
+#
+# This file is part of PyEPR.
+#
+# PyEPR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyEPR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyEPR.  If not, see <http://www.gnu.org/licenses/>.
 
 TEST_DATSET_URL = "http://earth.esa.int/services/sample_products/asar/IMP/ASA_IMP_1PNUPA20060202_062233_000000152044_00435_20529_3110.N1.gz"
 TEST_DATSET = tests/ASA_IMP_1PNUPA20060202_062233_000000152044_00435_20529_3110.N1
@@ -12,8 +30,8 @@ clean:
 	$(RM) src/*.py[co] tests/*.py[co] README.html
 
 distclean: clean
-	$(RM) -r build
-	$(RM) src/*.c src/*.o *.so
+	$(RM) -r build dist
+	$(RM) MANIFEST src/*.c src/*.o *.so
 	#$(RM) tests/*.N1
 
 check: epr.so $(TEST_DATSET)
--- setup.py
+++ setup.py
@@ -1,11 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011, Antonio Valentino <antonio.valentino@tiscali.it>
+#
+# This file is part of PyEPR.
+#
+# PyEPR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyEPR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyEPR.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension('epr', ['src/epr.pyx'], libraries=['epr_api'])]
 
 setup(
-  name = 'Python EPR API',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+    name='pyepr',
+    version='0.1',
+    author='Antonio Valentino',
+    author_email='antonio.valentino@tiscali.it',
+    url='https://github.com/avalentino/pyepr',
+    description='Python ENVISAT Product Reader API',
+    long_description='''PyEPR provides Python_ bindings for the ENVISAT
+Product Reader C API (`EPR API`_) for reading satellite data from ENVISAT_
+ESA_ (European Space Agency) mission.
+
+PyEPR, as well as the `EPR API`_ for C, supports ENVISAT_ MERIS, AATSR
+Level 1B and Level 2 and also ASAR data products. It provides access to
+the data either on a geophysical (decoded, ready-to-use pixel samples)
+or on a raw data layer. The raw data access makes it possible to read
+any data field contained in a product file.
+
+.. _Python: http://www.python.org
+.. _`EPR API`: https://github.com/bcdev/epr-api
+.. _ENVISAT: http://envisat.esa.int
+.. _ESA: http://earth.esa.int
+''',
+    #download_url='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Cython',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+    #platforms=,
+    license='GPL-3',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules=[Extension('epr', ['src/epr.pyx'], libraries=['epr_api'])],
 )
