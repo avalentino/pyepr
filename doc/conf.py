6af4ce7f97e7bb0904277f69632c8ace2f16e82e
Antonio Valentino <antonio.valentino@tiscali.it>
2011-03-30 22:52:01 +0200

    HTML and LaTeX documents now have the same source

--- doc/conf.py
+++ doc/conf.py
@@ -208,7 +208,7 @@ latex_font_size = '12pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('doc', 'pyepr.tex', u'PyEPR Documentation',
+  ('index', 'pyepr.tex', u'PyEPR Documentation',
    u'Antonio Valentino', 'manual'),
 ]
 
