85faddc141a14115396a7158217ef451cd17f9ad
Antonio Valentino <antonio.valentino@tiscali.it>
2011-08-11 18:16:03 +0200

    Use print function and unicode litersls in testing code

--- test/test_all.py
+++ test/test_all.py
@@ -18,6 +18,11 @@
 # You should have received a copy of the GNU General Public License
 # along with PyEPR.  If not, see <http://www.gnu.org/licenses/>.
 
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
 import os
 import re
 import sys
