5a431349e5a97d2a5b3ee3ac6d44979cc19c34c8
Antonio Valentino <antonio.valentino@tiscali.it>
2011-08-12 01:25:33 +0200

    Use new style division in test modules

--- test/test_all.py
+++ test/test_all.py
@@ -19,6 +19,7 @@
 # along with PyEPR.  If not, see <http://www.gnu.org/licenses/>.
 
 
+from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
