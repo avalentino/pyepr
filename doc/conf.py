aa6dd7f74cb6c4099d924c668278134fc9e2efca
Antonio Valentino <antonio.valentino@tiscali.it>
2011-08-12 02:04:13 +0200

    Updated version number (still dev)

--- doc/conf.py
+++ doc/conf.py
@@ -64,7 +64,7 @@ copyright = u'2011, Antonio Valentino'
 # built documents.
 #
 # The short X.Y version.
-version = '0.6.0'
+version = '0.6'
 # The full version, including alpha/beta/rc tags.
 release = version + 'dev'
 
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except ImportError:
 
 setup(
     name='pyepr',
-    version='0.6.0',
+    version='0.6',
     author='Antonio Valentino',
     author_email='antonio.valentino@tiscali.it',
     url='http://avalentino.github.com/pyepr',
