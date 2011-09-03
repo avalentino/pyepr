9ab9c50f30cddc5cfb5addb0a2d20ca940744fcf
Antonio Valentino <antonio.valentino@tiscali.it>
2011-09-03 13:43:06 +0200

    Revert "HTMK doc moved to readthedocs.org"
    This reverts commit f3b32382c6a0faa777e3731f663642a56f1bab70.

---
+++ html/_downloads/write_bands.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+# This program is a direct translation of the sample program
+# "write_bands.c" bundled with the EPR-API distribution.
+#
+# Source code of the C program is available at:
+# https://github.com/bcdev/epr-api/blob/master/src/examples/write_bands.c
+
+
+import os
+import sys
+import epr
+
+def write_raw_image(output_dir, product, band_name):
+    '''Generate the ENVI binary pattern image file for an actual DS.
+
+    The first parameter is the output directory path.
+
+    '''
+
+    # Build ENVI file path, DS name specifically
+    image_file_path = os.path.join(output_dir, band_name + '.raw')
+
+    band = product.get_band(band_name)
+    source_w = product.get_scene_width()
+    source_h = product.get_scene_height()
+    source_step_x = 1
+    source_step_y = 1
+
+    raster = band.create_compatible_raster(source_w, source_h,
+                                           source_step_x, source_step_y)
+
+    print 'Reading band "%s"...' % band_name
+    raster = band.read_raster(0, 0, raster)
+
+    out_stream = open(image_file_path, 'wb')
+
+    for line in raster.data:
+        out_stream.write(line.tostring())
+    # or better: raster.data.tofile(out_stream)
+
+    out_stream.close()
+
+    print 'Raw image data successfully written to "%s".' % image_file_path
+    print('C data type is "%s", element size %u byte(s), '
+          'raster size is %u x %u pixels.' % (
+                                epr.data_type_id_to_str(raster.data_type),
+                                raster.get_elem_size(),
+                                raster.get_width(),
+                                raster.get_height()))
+
+
+def main(*argv):
+    '''A program for converting producing ENVI raster information from
+    dataset.
+
+    It generates as many raster as there are dataset entrance parameters.
+
+    Call::
+
+        $ write_bands.py <envisat-product>
+                         <output directory for the raster file>
+                         <dataset name 1>
+                         [<dataset name 2> ... <dataset name N>]
+
+    Example::
+
+        $ write_bands.py \
+        MER_RR__1PNPDK20020415_103725_000002702005_00094_00649_1059.N1 \
+        . latitude
+
+    '''
+
+    if not argv:
+        argv = sys.argv
+
+    if len(argv) <= 3:
+        print('Usage: write_bands.py <envisat-product> <output-dir> '
+               '<dataset-name-1>')
+        print('                      [<dataset-name-2> ... <dataset-name-N>]')
+        print('  where envisat-product is the input filename')
+        print('  and output-dir is the output directory')
+        print('  and dataset-name-1 is the name of the first band to be '
+              'extracted (mandatory)')
+        print('  and dataset-name-2 ... dataset-name-N are the names of '
+              'further bands to be extracted (optional)')
+        print('Example:')
+        print('  write_bands MER_RR__2P_TEST.N1 . latitude')
+        print
+        sys.exit(1)
+
+    product_file_path = argv[1]
+    output_dir_path = argv[2]
+
+    # Open the product; an argument is a path to product data file
+    product = epr.open(product_file_path)
+
+    for band_name in argv[3:]:
+        write_raw_image(output_dir_path, product, band_name)
+
+if __name__ == '__main__':
+    main()
---
+++ html/_downloads/write_ndvi.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+# This program is a direct translation of the sample program
+# "write_ndvi.c" bundled with the EPR-API distribution.
+#
+# Source code of the C program is available at:
+# https://github.com/bcdev/epr-api/blob/master/src/examples/write_ndvi.c
+
+
+'''Example for using the epr-api
+
+Demonstrates how to open a MERIS L1b product and calculate the NDVI.
+
+This example does not demonstrate how to write good and safe code.
+It is reduced to the essentials for working with the epr-api.
+
+Calling sequence::
+
+    $ python write_ndvi.py <envisat-product> <output-file>
+
+for example::
+
+    $ python write_ndvi.py MER_RR__1P_test.N1 my_ndvi.raw
+
+'''
+
+import sys
+import struct
+import logging
+
+import epr
+
+
+def main(*argv):
+    if not argv:
+        argv = sys.argv
+
+    if len(argv) != 3:
+        print('Usage: write_ndvi <envisat-product> <output-file>')
+        print('  where envisat-product is the input filename')
+        print('  and output-file is the output filename.')
+        print('Example: MER_RR__1P_TEST.N1 my_ndvi.raw')
+        print
+        sys.exit(1)
+
+    # Open the product
+    product = epr.open(argv[1])
+
+    # The NDVI shall be calculated using bands 6 and 8.
+    band1_name = 'radiance_6'
+    band2_name = 'radiance_10'
+
+    band1 = product.get_band(band1_name)
+    band2 = product.get_band(band2_name)
+
+    # Allocate memory for the rasters
+    width = product.get_scene_width()
+    height = product.get_scene_height()
+    subsampling_x = 1
+    subsampling_y = 1
+    raster1 = band1.create_compatible_raster(width, height,
+                                             subsampling_x, subsampling_y)
+    raster2 = band2.create_compatible_raster(width, height,
+                                             subsampling_x, subsampling_y)
+
+    # Read the radiance into the raster.
+    offset_x = 0
+    offset_y = 0
+
+    logging.info('read "%s" data' % band1_name)
+    band1.read_raster(offset_x, offset_y, raster1)
+
+    logging.info('read "%s" data' % band2_name)
+    band2.read_raster(offset_x, offset_y, raster2)
+
+    # Open the output file
+    logging.info('write ndvi to "%s"' % argv[2])
+    out_stream = open(argv[2], 'wb')
+
+    # Loop over all pixel and calculate the NDVI.
+    #
+    # @NOTE: looping over data matrices is not the best soluton.
+    #        It is done here just for demostrative purposes
+    for j in range(height):
+        for i in range(width):
+            rad1 = raster1.get_pixel(i, j)
+            rad2 = raster2.get_pixel(i, j)
+            if (rad1 + rad2) != 0.0:
+                ndvi = (rad2 - rad1) / (rad2 + rad1)
+            else:
+                ndvi = -1.0
+            out_stream.write(struct.pack('f', ndvi))
+    logging.info('ndvi was written success')
+
+    out_stream.close()
+
+
+if __name__ == '__main__':
+    main()
