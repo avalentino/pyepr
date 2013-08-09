4473ef95a2ff3ba2861b8b41b1c8dcf90843fd47
Antonio Valentino <antonio.valentino@tiscali.it>
2013-08-09 18:30:57 +0200

    Ohloh widget added to the sidebar

--- doc/conf.py
+++ doc/conf.py
@@ -176,7 +176,7 @@ html_last_updated_fmt = '%b %d, %Y'
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
    'index': ['globaltoc.html', 'relations.html', 'sourcelink.html',
-             'searchbox.html', 'travis-ci.html'],
+             'searchbox.html', 'ohloh.html', 'travis-ci.html'],
 }
 
 # Additional templates that should be rendered to pages, maps page names to
