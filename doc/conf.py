9fc98d7da088ed5222a0b56bb9cd93701e78651a
Antonio Valentino <antonio.valentino@tiscali.it>
2013-08-19 18:00:10 +0200

    Silence sphinx warning

--- doc/conf.py
+++ doc/conf.py
@@ -80,7 +80,7 @@ release = version + 'dev'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'sphinxext']
+exclude_patterns = ['_build', 'sphinxext', '**/empty.txt']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
